2fa5fb9fe35bb13d838ffefac47da2ad767a38e8
Лизавета <@DESKTOP-ECBL7LS>
2022-01-26 21:50:18 +0300

    Добавьте файлы проекта.

---
+++ Лопушок/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Лопушок
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int start = 0;
+            int end = richTextBox1.Text.LastIndexOf(textBox1.Text);
+
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+
+            while (start < end)
+            {
+                richTextBox1.Find(textBox1.Text, start, richTextBox1.TextLength, RichTextBoxFinds.MatchCase);
+
+                richTextBox1.SelectionBackColor = Color.Yellow;
+
+                start = richTextBox1.Text.IndexOf(textBox1.Text, start) + 1;
+            }
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "")
+            {
+                
+             }
+
+        }
+    }
+}
